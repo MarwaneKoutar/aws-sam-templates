5f0faaae73b8abeb3bf2f90dd5b4e96702c33551
Marwane Koutar <marwane.koutar@student.hogent.be>
2024-02-28 18:27:41 +0100

    add makefile

--- templates/api-to-lambda/local_extensions.py
+++ templates/api-to-lambda/local_extensions.py
@@ -1,4 +1,5 @@
 import platform
+import os
 from cookiecutter.utils import simple_filter
 
 @simple_filter
@@ -6,5 +7,12 @@ def architecture(v):
     arch = platform.machine().lower()
     if 'aarch' in arch or 'arm' in arch:
         return 'arm64'
+    else:
+        return v
+    
+@simple_filter
+def get_abs_path(v):
+    if os.getcwd():
+        return os.getcwd()
     else:
         return v
--- templates/api-to-lambda/{{cookiecutter.__project_name}}/Sources/HelloWorld/LambdaHandler.swift
+++ templates/api-to-lambda/{{cookiecutter.__project_name}}/Sources/HelloWorld/LambdaHandler.swift
@@ -11,11 +11,6 @@ struct Response: Codable {
     let body: String
 }
 
-// Error type for unsupported requests
-enum APIError: Error {
-    case unsupportedRequest
-}
-
 // Entry point for the Lambda function
 @main
 struct HelloWorld: SimpleLambdaHandler {
@@ -23,13 +18,6 @@ struct HelloWorld: SimpleLambdaHandler {
     // Lambda Function handler
     func handle(_ event: Request, context: LambdaContext) async throws -> Response {
 
-        // Check if it's a GET request
-        if event.rawPath == "/" {
-            // Return a Hello message if it's a GET request
-            return Response(body: "Hello from AWS")
-        } else {
-            // Return a message indicating unsupported request
-            throw APIError.unsupportedRequest
-        }
+        return Response(body: "Hello, world!")
     }
 }
