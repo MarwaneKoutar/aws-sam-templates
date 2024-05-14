5aafab1a5065011ca0579cf7818c7174e0e6a050
Marwane Koutar <marwane.koutar@student.hogent.be>
2024-05-14 15:50:03 +0200

    Add headers

--- templates/scheduler-to-lambda/{{cookiecutter.__project_name}}/Sources/ScheduledTaskHandler/LambdaHandler.swift
+++ templates/scheduler-to-lambda/{{cookiecutter.__project_name}}/Sources/ScheduledTaskHandler/LambdaHandler.swift
@@ -1,3 +1,18 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the AWS Lambda Swift 
+// VSCode extension open source project.
+//
+// Copyright (c) 2024, the VSCode AWS Lambda Swift extension project authors.
+// Licensed under Apache License v2.0.
+//
+// See LICENSE.txt for license information
+// See CONTRIBUTORS.txt for the list of VSCode AWS Lambda Swift project authors
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+//===----------------------------------------------------------------------===//
+
 // Import the packages required by our function
 import AWSLambdaRuntime
 import AWSLambdaEvents
--- templates/sqs-to-lambda/{{cookiecutter.__project_name}}/Package.swift
+++ templates/sqs-to-lambda/{{cookiecutter.__project_name}}/Package.swift
@@ -1,6 +1,21 @@
 // swift-tools-version: {{ cookiecutter._swift_tools_version }}
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the AWS Lambda Swift 
+// VSCode extension open source project.
+//
+// Copyright (c) 2024, the VSCode AWS Lambda Swift extension project authors.
+// Licensed under Apache License v2.0.
+//
+// See LICENSE.txt for license information
+// See CONTRIBUTORS.txt for the list of VSCode AWS Lambda Swift project authors
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+//===----------------------------------------------------------------------===//
+
 import PackageDescription
 
 let package = Package(
